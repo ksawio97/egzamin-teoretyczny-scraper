b28fa33e1c323a9bbc2f7b358e271ba6335b424f
Ksawery Kuczyński <ksawerykuczynskikk@gmail.com>
2024-03-10 02:07:37 +0100

    Added parsing answers and correctIndex

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
+type Question struct {
+	title        string
+	answers      [4]string
+	correctIndex int
+}
+
 func main() {
-	// c := colly.NewCollector()
-	// c := colly.NewCollector()
 	c := colly.NewCollector()
+	questions := []Question{}
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
@@ -20,10 +25,35 @@ func main() {
 	})
 
 	c.OnHTML(".question", func(e *colly.HTMLElement) {
-		fmt.Println("Found ", e.ChildText(".title"))
+		title := e.ChildText(".title")
+		// expected only 4 answers
+		answers := [4]string{}
+		correctIndex := -1
+
+		e.ForEach("div.answer", func(i int, h *colly.HTMLElement) {
+			if i < len(answers) {
+				if h.DOM.HasClass("correct") {
+					correctIndex = i
+				}
+				answers[i] = h.Text
+			}
+		})
+
+		if correctIndex == -1 {
+			return
+		}
+
+		questions = append(questions, Question{
+			title:        title,
+			answers:      answers,
+			correctIndex: correctIndex,
+		})
 	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Printf("Scrape completed, found %v questions.\n", len(questions))
+	})
+
 	url := "https://www.praktycznyegzamin.pl/inf04/teoria/wszystko/"
 	c.Visit(url)
-
-	fmt.Println("Scraping finished")
 }
