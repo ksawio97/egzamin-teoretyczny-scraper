1d807a67a4f4a9cad407169158e03b94fb72e112
Ksawery Kuczyński <ksawerykuczynskikk@gmail.com>
2024-03-10 04:01:04 +0100

    Added saving images

--- main.go
+++ main.go
@@ -3,27 +3,44 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/gocolly/colly/v2"
 )
 
+// TODO add saving videos
 type Question struct {
 	Title        string    `json:"title"`
 	Answers      [4]string `json:"answers"`
 	CorrectIndex int       `json:"correctIndex"`
+	ImagePath    string    `json:"imagePath,omitempty"`
 }
 
 func main() {
-	c := colly.NewCollector()
-	questions := []Question{}
-	// Where questions will be saved
-	f, err := os.Create("_out/questions.json")
+	url := "https://www.praktycznyegzamin.pl/inf04/teoria/wszystko/"
+
+	// create output files
+	outputDir := "_out"
+	imagesPath := outputDir + "/images"
+	err := createFoldersStructure(outputDir, imagesPath)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("failed to create folders structure: %v", err)
 		return
 	}
-	defer f.Close()
+
+	f, err := os.Create(outputDir + "/questions.json")
+	if err != nil {
+		fmt.Printf("failed to create questions output file: %v", err)
+		return
+	}
+
+	c := colly.NewCollector()
+	client := &http.Client{}
+
+	questions := []Question{}
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
@@ -48,6 +65,25 @@ func main() {
 			}
 		})
 
+		imgSrc := e.ChildAttr("img", "src")
+		imgName := ""
+		if imgSrc != "" {
+			// parse file name from imgsrc
+			pattern, _ := regexp.Compile(`[^/]+$`)
+			imgName = string(pattern.Find([]byte(imgSrc)))
+			if imgName == "" {
+				fmt.Printf("failed parsing file name from %v", imgSrc)
+				return
+			}
+
+			imgPath := imagesPath + "/" + string(imgName)
+			err := saveFileFromUrl(client, fmt.Sprintf("%v/%v", url, imgSrc), imgPath)
+			if err != nil {
+				fmt.Println(err)
+				imgName = ""
+			}
+		}
+
 		if correctIndex == -1 {
 			return
 		}
@@ -56,6 +92,7 @@ func main() {
 			Title:        title,
 			Answers:      answers,
 			CorrectIndex: correctIndex,
+			ImagePath:    imgName,
 		})
 	})
 
@@ -70,6 +107,52 @@ func main() {
 		f.Write(data)
 	})
 
-	url := "https://www.praktycznyegzamin.pl/inf04/teoria/wszystko/"
 	c.Visit(url)
+}
+
+func createFoldersStructure(outputDir, imagesPath string) error {
+	// Create file where questions will be saved
+	err := os.MkdirAll(outputDir, 0777)
+	if err != nil {
+		return fmt.Errorf("failed creating %v file: %v", outputDir, err)
+	}
+
+	// Create dir where images will be saved
+	err = os.MkdirAll(imagesPath, 0777)
+	if err != nil {
+		return fmt.Errorf("failed creating %v directory: %v", imagesPath, err)
+	}
+	return nil
+}
+
+func saveFileFromUrl(client *http.Client, url, path string) error {
+	body, err := getFileFromUrl(client, url)
+	if err != nil {
+		return fmt.Errorf("failed to get file from url %v: %v", url, err)
+	}
+	defer body.Close()
+
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %v", err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, body)
+	if err != nil {
+		return fmt.Errorf("failed to copy body to new file %v: %v", path, err)
+	}
+	return nil
+}
+
+func getFileFromUrl(client *http.Client, url string) (io.ReadCloser, error) {
+	response, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode == http.StatusOK {
+		return response.Body, nil
+	}
+	response.Body.Close()
+	return nil, fmt.Errorf("failed to get file: http status code %v", response.StatusCode)
 }
